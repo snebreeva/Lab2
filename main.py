7ba8c9562a578d32529eef0516762178ff2ea788
snebreeva <91993969+snebreeva@users.noreply.github.com>
2021-10-20 22:41:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-import decimal
-from math import factorial
+from decimal import Decimal as de
+from decimal import *
 
-def pi(n):
-  decimal.getcontext().prec = n + 1
-  p = 0
-  k = 0
-  x = 426880 * decimal.Decimal('10005').sqrt()
-  a = factorial(6 * p)
-  b = 545140134 * p + 13591409
-  d = a * b
-  c = factorial(3 * p)
-  f = factorial(p) ** 3
-  e = (-262537412640768000) ** p
-  y = decimal.Decimal(c * f * e)
-  k += d / y
-  return print(x / k)
+n = int(input())
+getcontext().prec = n + 1
+
+def pin(n):
+    pi = de(13591409)
+    ak = de(1)
+    k = 1
+    while k < n:
+        ak *= -de((6 * k - 5) * (2 * k - 1) * (6 * k - 1)) / de(k * k * k * 26680 * 640320 * 640320)
+
+        val = ak * (13591409 + 545140134 * k)
+
+        d = de((6 * k - 5) * (2 * k - 1) * (6 * k - 1)) / de(k * k * k * 26680 * 640320 * 640320)
+
+        pi += val
+        k += 1
+    pi = pi * de(10005).sqrt() / 4270934400
+    pi = pi ** (-1)
+    return pi
+
+print(pin(n))
